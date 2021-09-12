5d66a48fde4114e36b0b94914ca8639cc19ffdba
HassanBahati <mukisabahati@gmail.com>
2021-09-12 13:05:08 +0300

    added aarticle css

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,22 @@ export default function Home() {
     <div>
       <Head>
         <title>WebDev News</title>
-        <meta name="keywords" content="web programming , development"/>
+        <meta name="keywords" content="web programming , development" />
       </Head>
-
 
       <h1>Welcome to Next</h1>
     </div>
   );
 }
+
+// fetch data from external api
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.cpm/post?_limit=6`);
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
